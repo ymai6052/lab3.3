f185ba55ae3400718951e8287eb92829674a68d7
BT_1N3_29 <BT_1N3_29@BT_1N3_29.bktech.local>
2017-11-28 12:43:47 -0500

    48498

--- lab3.3/src/functions.java
+++ lab3.3/src/functions.java
@@ -5,12 +5,16 @@ public class functions {
 	{
 		int [] nums = {4,7,9,2,6};
 		double [] nums2 = {11.8,7.1,3.9,0.1,5.6,9.8};
-		String [] string1= {"a","v","d","b","c"};
-		bubbleSort(string1);
+		String [] string1= {"v","a","d","b","c"};
+		
 		//insertionSort(nums);
-		printArraystring(string1);
+		//printArrayint(nums);
+		
 		//selectionSort(nums2);
 		//printArraydouble(nums2);
+		
+		bubbleSort(string1);
+		printArraystring(string1);
 	}
 	public static void insertionSort(int[] list1)
 	{
@@ -21,9 +25,7 @@ public class functions {
 			{
 				if(list1[j] < list1[j-1])
 				{
-					temp = list1[j];
-					list1[j] = list1[j-1];
-					list1[j-1] = temp;
+					swap(list1,j,j-1);
 				}
 			}
 		}
@@ -49,35 +51,31 @@ public class functions {
 	public static void bubbleSort(String [] list1)
 	{
 		String low=list1[0];
-		int swapnum=1;
-		while(swapnum>0)
+		boolean swapped=true;
+		while(swapped)
 		{
-			swapnum=0;
-			for(int i=1;i<list1.length;i++)
+			int counter=0;
+			for(int i=0;i<list1.length-1;i++)
 			{ 
-				if(low.compareTo(list1[i])>0)
+				if(list1[i].compareTo(list1[i+1])>0)
 				{
-					low=list1[i];
-				}
-				else if(low.compareTo(list1[i])<0)
-				{
-					swapstring(list1,i,i++);
-					swapnum++;
-				}
-				else
-				{
-					//i++;
+					swapstring(list1,i+1,i);
+					counter++;
 				}
 			}
+			if(counter==0)
+			{
+				swapped=false;
+			}
 		}
-		
 	}
 	public static void printArrayint(int[] arr)
 	{
-		for(int i=0;i<arr.length;i++)
+		for(int i=0;i<arr.length-1;i++)
 		{
 			System.out.print(arr[i]+",");
 		}
+		System.out.print(arr[arr.length-1]);
 	}
 	public static void printArraystring(String[] arr)
 	{
@@ -94,6 +92,12 @@ public class functions {
 			System.out.print(arr[i]+",");
 		}
 		System.out.print(arr[arr.length-1]);
+	}
+	public static void swap(int[]arr,int i,int j)
+	{
+		int x = arr[i];
+		arr[i] = arr[j];
+		arr[j] = x;
 	}
 	public static void swap(double[]arr,int i,int j)
 	{
